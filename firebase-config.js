9e3f5012970e3b8504f964664234c32b275cc2cf
Gabriel Arroyo <gabriel_eleazar.arroyo@daimler.com>
2022-07-05 16:02:58 -0500

    add firebase

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoSt5MX460l1HpklUaXh4Ax6r8nw0hLdk",
+  authDomain: "uzsports.firebaseapp.com",
+  databaseURL: "https://uzsports-default-rtdb.firebaseio.com",
+  projectId: "uzsports",
+  storageBucket: "uzsports.appspot.com",
+  messagingSenderId: "972184072963",
+  appId: "1:972184072963:web:4a59b0415316fe29862f36",
+  measurementId: "G-9L8YVZ1VEE",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Test from "./components/test";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div>React App</div>
+    <Test />
   </React.StrictMode>
 );
