6448983f44484097815aa9ae3894a70794f80bc2
Gabriel Arroyo <gabriel_eleazar.arroyo@daimler.com>
2022-07-09 13:45:12 -0500

    material ui baseline

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import { Button } from "@mui/material";
 
 const Layout = () => {
   return (
@@ -7,7 +8,12 @@ const Layout = () => {
       <div>
         <h1>Navbar</h1>
         <nav>
-          <Link to="/">Home</Link>| <Link to="account/register">Register</Link>|{" "}
+          <Link to="/" sx={{ textDecoration: "none" }}>
+            <Button variant="contained" sx={{ textDecoration: "none" }}>
+              Home
+            </Button>
+          </Link>
+          | <Link to="account/register">Register</Link>|{" "}
           <Link to="account/login">Login</Link>
         </nav>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -12,30 +12,36 @@ import Account from "./pages/account/account";
 import AccountLayout from "./components/layout/account-layout";
 import NotFoundPage from "./pages/404/404";
 import { HashRouter } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline ";
+import { theme } from "./styles/theme";
+import { ThemeProvider } from "@mui/material/styles";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Account />} />
-          <Route path="*" element={<></>} />
-        </Routes>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="account" element={<AccountLayout />}>
-              <Route index element={<Account />} />
-              <Route path="register/*" element={<Register />} />
-              <Route path="login" element={<Login />} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <QueryClientProvider client={queryClient}>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Account />} />
+            <Route path="*" element={<></>} />
+          </Routes>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="account" element={<AccountLayout />}>
+                <Route index element={<Account />} />
+                <Route path="register/*" element={<Register />} />
+                <Route path="login" element={<Login />} />
+              </Route>
             </Route>
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </HashRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </HashRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
