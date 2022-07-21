0c5f159babef231bea689fc01fd8e35993a49c5d
Gabriel Arroyo <gabriel_eleazar.arroyo@daimler.com>
2022-07-21 12:36:59 -0500

    template light green

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -1,10 +1,10 @@
 import { createTheme } from "@mui/material/styles";
-import { blue } from "@mui/material/colors";
+import { lightGreen } from "@mui/material/colors";
 
 export const theme = createTheme({
   palette: {
     primary: {
-      main: blue[500],
+      main: lightGreen[500],
     },
     secondary: {
       main: "#f44336",
